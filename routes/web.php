99500db1a14dc8daa3da1ce950c415dbe989e978
Shamima Haque <haqueshamima34@gmail.com>
2024-07-14 20:04:01 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Backend\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', [DashboardController::class,'index'])->name('admin.dashboard');
+});
